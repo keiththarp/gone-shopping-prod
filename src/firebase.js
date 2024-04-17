571e8d053453fb7888d16b339305996157739862
keiththarp <keithstharp@gmail>
2024-04-17 19:46:43 -0400

    revert to see if it's fixed

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDv7674DzPT-ujhyu6jancDR-BVKNeYQmI",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
